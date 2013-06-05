d1ca9d1b74804da53512e599426986cd77e113cf
Philipp Eder <ederphpp@gmail.com>
2013-06-05 09:45:29 +0200

    removed unnecessary scripts

--- getAccessToken.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-curl --user tonr:secret -X POST -d "code=$@&grant_type=authorization_code&redirect_uri=http://localhost:8080/oauth2-client/accessToken" http://localhost:8080/authorization-server/oauth/token
-echo -e "\n"
