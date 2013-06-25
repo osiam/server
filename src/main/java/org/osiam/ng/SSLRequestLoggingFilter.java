3ae98aec214f9cf1a28380787b02608dcc465424
jtodea <jochen.todea@googlemail.com>
2013-06-25 16:30:08 +0200

    OSNG-111 - added filter to log a message if TLS is not enabled

---
+++ src/main/java/org/osiam/ng/SSLRequestLoggingFilter.java
@@ -0,0 +1,34 @@
+package org.osiam.ng;
+
+import org.apache.log4j.Logger;
+
+import javax.servlet.*;
+import java.io.IOException;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: jtodea
+ * Date: 25.06.13
+ * Time: 15:33
+ * To change this template use File | Settings | File Templates.
+ */
+public class SSLRequestLoggingFilter implements Filter {
+
+    Logger  logger = Logger.getLogger(SSLRequestLoggingFilter.class.getName());
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        if (request.getScheme().equals("http")) {
+            logger.warn("SSL/TLS should be used");
+        }
+        chain.doFilter(request, response);
+    }
+
+    @Override
+    public void destroy() {
+    }
+}
