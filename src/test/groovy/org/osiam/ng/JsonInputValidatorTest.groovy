836254112b846cd3b0a0b77880026c325b7c3764
jtodea <jochen.todea@googlemail.com>
2013-06-27 12:48:10 +0200

    OSNG-87 - added validator class and test to check the json violations

---
+++ src/test/groovy/org/osiam/ng/JsonInputValidatorTest.groovy
@@ -0,0 +1,13 @@
+package org.osiam.ng
+
+import spock.lang.Specification
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: jtodea
+ * Date: 27.06.13
+ * Time: 12:08
+ * To change this template use File | Settings | File Templates.
+ */
+class JsonInputValidatorTest extends Specification {
+}
