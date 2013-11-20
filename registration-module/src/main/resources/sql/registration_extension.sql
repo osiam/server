5cc0708aa23a7dbb3b4263d7d1868315e2f4b54d
jtodea <jochen.todea@googlemail.com>
2013-11-20 11:27:18 +0100

    added extension fields for change email functionality and configuration parameter for the confirmation link

--- registration-module/src/main/resources/sql/registration_extension.sql
+++ registration-module/src/main/resources/sql/registration_extension.sql
@@ -1,7 +1,7 @@
 --
 -- Updating hibernate sequence start value
 --
-SELECT pg_catalog.setval('hibernate_sequence', 9, false);
+SELECT pg_catalog.setval('hibernate_sequence', 11, false);
 
 --
 -- Extension data for registration purpose
@@ -11,3 +11,5 @@ Insert INTO scim_extension VALUES (6, 'urn:scim:schemas:osiam:1.0:Registration')
 
 INSERT INTO scim_extension_field VALUES (7, false, 'activationToken', 'STRING', 6);
 INSERT INTO scim_extension_field VALUES (8, false, 'oneTimePassword', 'STRING', 6);
+INSERT INTO scim_extension_field VALUES (9, false, 'emailConfirmToken', 'STRING', 6);
+INSERT INTO scim_extension_field VALUES (10, false, 'tempMail', 'STRING', 6);
