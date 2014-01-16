824a03fed41dcc1aca39559cd3f190ee4f35ca4e
Thomas Krille <tkrille@gmail.com>
2014-01-16 17:18:13 +0100

    [fix] changed sql scripts to pin down table field order

--- registration-module/src/main/resources/sql/registration_extension.sql
+++ registration-module/src/main/resources/sql/registration_extension.sql
@@ -1,15 +1,19 @@
 --
 -- Updating hibernate sequence start value
 --
-SELECT pg_catalog.setval('hibernate_sequence', 9, false);
+SELECT pg_catalog.setval('hibernate_sequence', 10, false);
 
 --
 -- Extension data for registration purpose
 --
 
-Insert INTO scim_extension VALUES (4, 'urn:scim:schemas:osiam:1.0:Registration');
+Insert INTO scim_extension VALUES (5, 'urn:scim:schemas:osiam:1.0:Registration');
 
-INSERT INTO scim_extension_field VALUES (5, false, 'activationToken', 'STRING', 4);
-INSERT INTO scim_extension_field VALUES (6, false, 'oneTimePassword', 'STRING', 4);
-INSERT INTO scim_extension_field VALUES (7, false, 'emailConfirmToken', 'STRING', 4);
-INSERT INTO scim_extension_field VALUES (8, false, 'tempMail', 'STRING', 4);
+INSERT INTO scim_extension_field (internal_id, is_required, name, type, extension_internal_id)
+	VALUES (6, false, 'activationToken', 'STRING', 5);
+INSERT INTO scim_extension_field (internal_id, is_required, name, type, extension_internal_id)
+	VALUES (7, false, 'oneTimePassword', 'STRING', 5);
+INSERT INTO scim_extension_field (internal_id, is_required, name, type, extension_internal_id)
+	VALUES (8, false, 'emailConfirmToken', 'STRING', 5);
+INSERT INTO scim_extension_field (internal_id, is_required, name, type, extension_internal_id)
+	VALUES (9, false, 'tempMail', 'STRING', 5);
